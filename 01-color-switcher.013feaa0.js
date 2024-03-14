320811eafa0d6a42ab51b49940975e96ac3574ba
Underskinua <102025924+Underskinua@users.noreply.github.com>
2024-03-14 15:52:46 +0000

    Deploying to gh-pages from @ Underskinua/goit-js-hw-09@21d8867ad3c6e600e5cf423221bfecc0e1938d1e 🚀

---
+++ 01-color-switcher.013feaa0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=null;function o(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){a||(a=setInterval(o,1e3),e.disabled=!0)})),n.addEventListener("click",(function(){clearInterval(a),a=null,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.013feaa0.js.map
---
+++ 01-color-switcher.0c40c816.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");let l=null;function r(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(()=>{l||(l=setInterval(r,1e3),e.disabled=!0)})),a.addEventListener("click",(()=>{clearInterval(l),l=null,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.0c40c816.js.map
