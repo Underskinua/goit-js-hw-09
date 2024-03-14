21d8867ad3c6e600e5cf423221bfecc0e1938d1e
Underskinua <zovadim@gmail.com>
2024-03-14 17:52:16 +0200

    taskOne

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', () => {
+  if (intervalId) {
+    return;
+  }
+
+  intervalId = setInterval(changeColor, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  startBtn.disabled = false;
+});
