cd280a5abb6737598814e1902a4a2ed4d6fa3c71
Underskinua <zovadim@gmail.com>
2024-03-14 19:32:53 +0200

    addTask_three

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,26 @@
 const body = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
+//переменная для хранения идентификатора интервала.
+let intervalId = null; 
 
-let intervalId = null;
-
+//Генерируем случайный цвет.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-
+// Функция меняет цвет фон `body` на случайный.
 function changeColor() {
   body.style.backgroundColor = getRandomHexColor();
 }
-
+// Обработчик события "click" для кнопки "Start".
 startBtn.addEventListener('click', () => {
+  // Проверка, запущен ли интервал.
   if (intervalId) {
     return;
   }
-
+  // Запускает интервал с интервалом 1 секунда.
   intervalId = setInterval(changeColor, 1000);
+  // Робить кнопку "Start" неактивною.
   startBtn.disabled = true;
   stopBtn.disabled = false;
 });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+
+    const shouldResolve = Math.random() > 0.3; // Случайное решение о выполнении или отклонении промиса
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay }); // Выполнение промиса с объектом данных
+
+      } else {
+        reject({ position, delay }); // Отклонение промиса с объектом данных
+
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Предотвращение стандартного поведения формы
+
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  const promises = []; // Массив для хранения созданных промисов
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = delay + i * step; // Расчет текущей задержки с учетом шага
+
+    promises.push(createPromise(i + 1, currentDelay)); // Добавление промиса в массив
+  }
+
+  promises.forEach((promise) => {
+    promise
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  });
+});
